fd6c159c1086db5a8c00213e958f7aff2a7afd0a
Busarang <riyukiara@gmail.com>
2025-03-22 13:24:46 +0800

    initial login page view and url

--- Group3_AttendanceSystem/AttendanceSystem/Index/views.py
+++ Group3_AttendanceSystem/AttendanceSystem/Index/views.py
@@ -4,7 +4,12 @@ from django.views import View
 # Create your views here.
 
 class Home(View):
-    template_name = 'HomePage.html'
 
     def get(self,request):
         return render(request, 'HomePage.html')
+
+
+class Login(View):
+
+    def get(self,request):
+        return render(request, 'login.html')
