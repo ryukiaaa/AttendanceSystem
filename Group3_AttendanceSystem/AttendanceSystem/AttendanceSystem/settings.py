ecab1be49071d9fbc78b4f93641ede665dd8db51
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-03-29 10:33:25 +0800

    Update settings.py

--- Group3_AttendanceSystem/AttendanceSystem/AttendanceSystem/settings.py
+++ Group3_AttendanceSystem/AttendanceSystem/AttendanceSystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,8 +83,8 @@ DATABASES = {
         'NAME': 'dbattendancesystem',
         'USER': 'root',
         'PASSWORD': 'password',
-        'host': '127.0.0.1',
-        'port': '3306',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'OPTIONS': {'init_command':"set sql_mode='strict_trans_tables'"},
     }
 }
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
