5e89159fcf6eb81327a82f54813b2c8f07a69e64
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-04 03:53:07 +0800

    Updated Teacher/models.py

--- Group3_AttendanceSystem/AttendanceSystem/Teacher/models.py
+++ Group3_AttendanceSystem/AttendanceSystem/Teacher/models.py
@@ -1,24 +1,46 @@
 from django.db import models
-from Index.models import User
+from django.contrib.auth.models import User
+
+# Importing model 'Student' causes an ImportError:
+# "cannot import name 'Student' from partially initialized module 'Student.models'
+# (most likely due to a circular import)"
+# RESOLVED: On line 42, used 'Appname.Modelname' as parameter instead
+# from Student.models import Student
 
 # Create your models here.
-
 class Teacher(User):
-    teacher_id = models.CharField(max_length=20, primary_key=True)
     department = models.CharField(max_length=100)
 
+    # Makes sure the password is hashed in the database when a new teacher is created
+    def save(self, *args, **kwargs):
+        self.set_password(self.password)
+        super().save(*args, **kwargs)
+
+    # Show firstname and lastname in admin site
     def __str__(self):
-        return f"{self.teacher_id}-{self.last_name}"
+        # Get inherited fields
+        first_name = super(User, self).first_name
+        last_name = super(User, self).last_name
+
+        return first_name + ' ' + last_name
 
 
 class Class(models.Model):
-    class_id = models.CharField(max_length=20, primary_key=True)
+    class_id = models.IntegerField(primary_key=True)
     class_name = models.CharField(max_length=100)
     class_code = models.CharField(max_length=20, unique=True)
+    join_code = models.CharField(max_length=50, unique=True) # Could be converted to a QR code (maybe?)
     building_name = models.CharField(max_length=100)
     capacity = models.PositiveIntegerField()
-    schedule = models.CharField(max_length=100)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='classes')
+    schedule1 = models.CharField(max_length=100)
+    schedule2 = models.CharField(max_length=100)
 
+    # ONE-TO-MANY RELATIONSHIP: A teacher handles many classes
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='handles')
+
+    # MANY-TO-MANY RELATIONSHIP: A class has many students and students join many classes
+    students = models.ManyToManyField('Student.Student', through='Student.Attendance', related_name='joins')
+
+    # Show class code in the admin site
     def __str__(self):
         return self.class_code
