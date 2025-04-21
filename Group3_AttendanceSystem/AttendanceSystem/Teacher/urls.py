2ca6f63b5b63f969405928c817ad6f328c8161d2
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:52:52 +0800

    Update urls.py

--- Group3_AttendanceSystem/AttendanceSystem/Teacher/urls.py
+++ Group3_AttendanceSystem/AttendanceSystem/Teacher/urls.py
@@ -1,6 +1,13 @@
+# Teacher URLs
+
+from django.contrib.auth.decorators import permission_required
 from django.urls import path
-from .views import Dashboard
+from . import views
+
+app_name = 'teacher'
 
 urlpatterns = [
-    path('dashboard', Dashboard.as_view(), name='dashboard'),
+    path('', permission_required('Teacher.view_teacher', raise_exception=True)(views.TeacherDashboard.as_view()), name='dashboard'),
+    path('createclass/', permission_required('Teacher.view_teacher', raise_exception=True)(views.CreateClass.as_view()), name='createclass'),
+    path('logout/', views.Logout.as_view(), name='logout'),
 ]
