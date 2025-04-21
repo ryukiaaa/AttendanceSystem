cec69494019a3cddaebdda791657e2eb65a94908
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:53:52 +0800

    Update views.py

--- Group3_AttendanceSystem/AttendanceSystem/Teacher/views.py
+++ Group3_AttendanceSystem/AttendanceSystem/Teacher/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+# Teacher Views
+
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth import logout
+from . import models
+from . import forms
 
 
-# Create your views here.
+class TeacherDashboard(View):
+    def get(self, request):
+        teacher = request.user
+        return render(request, 'teacher_dashboard.html', {'teacher':teacher})
 
-class Dashboard(View):
 
-    def get(self,request):
-        return render(request, 'TeacherDashboard.html')
+class CreateClass(View):
+    form = forms.CreateClassForm()
+
+    def get(self, request):
+        teacher = request.user
+        return render(request, 'create_class.html', {'form':self.form, 'teacher':teacher})
+
+    def post(self, request):
+        teacher = request.user
+
+        if request.method == 'POST':
+            form = forms.CreateClassForm(request.POST)
+
+            # Adds user to the database if all form fields are valid
+            if form.is_valid():
+                new_class = form.save(commit=False)
+                # Assign currently logged in teacher to the class
+                new_class.teacher = models.Teacher.objects.get(user_ptr_id=request.user)
+                new_class.save()
+                return render(request, 'create_class.html', {'form': self.form, 'teacher':teacher})
+
+        return None
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('index')
