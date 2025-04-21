0bad8ef3d51fc4f85232b1187cdff95f1c35b480
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:39:17 +0800

    Update urls.py

--- Group3_AttendanceSystem/AttendanceSystem/Student/urls.py
+++ Group3_AttendanceSystem/AttendanceSystem/Student/urls.py
@@ -1,10 +1,12 @@
 # Student URLs
-from django.urls import path
+
 from django.contrib.auth.decorators import permission_required
+from django.urls import path
 from . import views
 
 app_name = 'student'
 
 urlpatterns = [
-    path('dashboard/', permission_required('Student.view_student', raise_exception=True)(views.StudentDashboard.as_view()), name='dashboard')
+    path('dashboard/', permission_required('Student.view_student', raise_exception=True)(views.StudentDashboard.as_view()), name='dashboard'),
+    path('dashboard/logout/', views.Logout.as_view(), name='logout'),
 ]
