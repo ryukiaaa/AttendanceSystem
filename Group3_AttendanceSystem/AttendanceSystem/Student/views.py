e8a536630326ac11dc417c288d7eeb38c74ac01a
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:39:33 +0800

    Update views.py

--- Group3_AttendanceSystem/AttendanceSystem/Student/views.py
+++ Group3_AttendanceSystem/AttendanceSystem/Student/views.py
@@ -1,7 +1,16 @@
 # Student Views
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import logout
+
 
 class StudentDashboard(View):
     def get(self, request):
-        return render(request, 'studentdashboard.html')
+        return render(request, 'student_dashboard.html')
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('index')
