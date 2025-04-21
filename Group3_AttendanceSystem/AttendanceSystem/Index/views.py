e8a38c28eac4e66a80ca5f811b48cfe80ba01347
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:33:41 +0800

    Update views.py
    Views will now check for the group that a user belongs so that they can be redirected to proper pages. Views will also use this to deny users from accessing pages they shouldn't have access to.

--- Group3_AttendanceSystem/AttendanceSystem/Index/views.py
+++ Group3_AttendanceSystem/AttendanceSystem/Index/views.py
@@ -1,20 +1,30 @@
-# Authentication Views
 from django.shortcuts import render, redirect
-from django.views import View
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import Group
+from django.views import View
 from . import forms
 
+# Create your views here.
 
 class Index(View):
     def get(self, request):
-        return render(request, 'index.html')
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            return redirect('student:dashboard')
+        elif request.user.is_authenticated and request.user.groups.filter(name='Teacher').exists():
+            return redirect('teacher:dashboard')
+        else:
+            return render(request, 'index.html')
 
 
 class Login(View):
     def get(self, request):
-        return render(request, 'login.html')
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            return redirect('student:dashboard')
+        elif request.user.is_authenticated and request.user.groups.filter(name='Teacher').exists():
+            return redirect('teacher:dashboard')
+        else:
+            return render(request, 'login.html')
 
     def post(self, request):
         if request.method == 'POST':
@@ -28,53 +38,34 @@ class Login(View):
                 return redirect('student:dashboard')
             elif user is not None and user.groups.filter(name='Teacher').exists():
                 login(request, user)
-                return redirect('index')
+                return redirect('teacher:dashboard')
             else:
                 messages.info(request, 'Invalid credentials')
                 return render(request, 'login.html')
 
+        return None
+
 
 class Register(View):
     def get(self, request):
-        return render(request, 'register.html')
-
-
-# UNUSED: Old register page (creates supertype User)
-class RegisterOld(View):
-    form = forms.CreateUserForm()
-
-    def get(self, request):
-        return render(request, 'registerold.html', {'form':self.form})
-
-    def post(self, request):
-        if request.method == 'POST':
-            form = forms.CreateUserForm(request.POST)
-
-            # Adds user to the database if all form fields are valid
-            if form.is_valid():
-                form.save()
-                firstname = form.cleaned_data.get('first_name')
-                lastname = form.cleaned_data.get('last_name')
-                messages.success(request, 'New user ' + firstname + ' ' + lastname + ' has been created')
-                return redirect('login')
-            # If not, show error message and refresh the reset form fields
-            else:
-                messages.error(request, 'ERROR: All fields must be valid!')
-
-                if len(form.cleaned_data.get('password')) < 8:
-                    messages.error(request, 'Password must contain at least 8 characters')
-
-                if form.cleaned_data.get('password') != form.cleaned_data.get('password2'):
-                    messages.error(request, 'Passwords should match')
-
-                return render(request, 'registerold.html', {'form': self.form})
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            return redirect('student:dashboard')
+        elif request.user.is_authenticated and request.user.groups.filter(name='Teacher').exists():
+            return redirect('teacher:dashboard')
+        else:
+            return render(request, 'register.html')
 
 
 class RegisterStudent(View):
     form = forms.CreateStudentForm()
 
     def get(self, request):
-        return render(request, 'registerstudent.html', {'form': self.form})
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            return redirect('student:dashboard')
+        elif request.user.is_authenticated and request.user.groups.filter(name='Teacher').exists():
+            return redirect('teacher:dashboard')
+        else:
+            return render(request, 'register_student.html', {'form': self.form})
 
     def post(self, request):
         if request.method == 'POST':
@@ -99,14 +90,21 @@ class RegisterStudent(View):
                 if form.cleaned_data.get('password') != form.cleaned_data.get('password2'):
                     messages.error(request, 'Passwords should match')
 
-                return render(request, 'registerstudent.html', {'form': self.form})
+                return render(request, 'register_student.html', {'form': self.form})
+
+        return None
 
 
 class RegisterTeacher(View):
     form = forms.CreateTeacherForm()
 
     def get(self, request):
-        return render(request, 'registerteacher.html', {'form': self.form})
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            return redirect('student:dashboard')
+        elif request.user.is_authenticated and request.user.groups.filter(name='Teacher').exists():
+            return redirect('teacher:dashboard')
+        else:
+            return render(request, 'register_teacher.html', {'form': self.form})
 
     def post(self, request):
         if request.method == 'POST':
@@ -115,7 +113,7 @@ class RegisterTeacher(View):
             # Adds user to the database if all form fields are valid
             if form.is_valid():
                 user = form.save()
-                # Assign to 'Teacher' group to access student pages
+                # Assign to 'Teacher' group to access teacher pages
                 user.groups.add(Group.objects.get(name='Teacher'))
                 firstname = form.cleaned_data.get('first_name')
                 lastname = form.cleaned_data.get('last_name')
@@ -131,4 +129,6 @@ class RegisterTeacher(View):
                 if form.cleaned_data.get('password') != form.cleaned_data.get('password2'):
                     messages.error(request, 'Passwords should match')
 
-                return render(request, 'registerteacher.html', {'form': self.form})
+                return render(request, 'register_teacher.html', {'form': self.form})
+
+        return None
