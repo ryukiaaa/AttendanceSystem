253297d595cd815508ef845ed7f5204c6e5bb863
Mortymier <113320224+Mortymier@users.noreply.github.com>
2025-04-22 02:29:52 +0800

    Update urls.py

--- Group3_AttendanceSystem/AttendanceSystem/Index/urls.py
+++ Group3_AttendanceSystem/AttendanceSystem/Index/urls.py
@@ -1,11 +1,11 @@
-# Authentication URLs
+# Index URLs
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'), #localhost:8000
-    path('login/', views.Login.as_view(), name='login'), #localhost:8000/login
-    path('register/', views.Register.as_view(), name='register'), #localhost:8000/register
-    path('register/student/', views.RegisterStudent.as_view(), name='registerstudent'), #localhost:8000/register/student
-    path('register/teacher/', views.RegisterTeacher.as_view(), name='registerteacher'), #localhost:8000/register/teacher
+    path('', views.Index.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('register/student/', views.RegisterStudent.as_view(), name='registerstudent'),
+    path('register/teacher/', views.RegisterTeacher.as_view(), name='registerteacher'),
 ]
